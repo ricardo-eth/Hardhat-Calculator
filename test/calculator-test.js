ed6e586478c3ebe62c5c741dd0522c4c4f60b3c6
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-05-20 18:59:02 +0200

    Up

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,11 +1,13 @@
 const { expect } = require("chai");
 
 describe("Calculator", function () {
-  it("should calculate each method", async function () {
-    const Calculator = await ethers.getContractFactory("Calculator");
-    const calculator = await Calculator.deploy("Hello, calculator!");
+  it("Should calculate the right result : addition", async function () {
+    const result = await calculator.add(10, 10);
+    expect(result).to.equal(20);
+  });
 
-    await calculator.deployed();
-    expect(await calculator.add(10, 5));
+  it("Should calculate the right result : substraction", async function () {
+    const result = await calculator.sub(10, 5);
+    expect(result).to.equal(5);
   });
 });
